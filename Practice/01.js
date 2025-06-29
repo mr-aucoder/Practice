758d0f387dd731636ed3c6f753a90d8d74a14d0a
mr-aucoder <fakelove112113@gmail.com>
2025-06-29 07:42:21 +0000

    IIFE

---
+++ Practice/01.js
@@ -0,0 +1,8 @@
+// ++++++++++++++++++++++++++ IIFE ++++++++++++++++++++
+// Immediately Invoked Function Expressions
+( function one(){
+   console.log("Hello world");
+})();
+( ()=>{
+   console.log("Wrold Hello");
+})();
